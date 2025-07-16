879d784613ff88c531d109b60428a85e528b0b7d
Luca5Eckert <ecklukatv@gmail.com>
2025-07-16 20:08:17 -0300

    #1 chore: integra projeto base com Spring Initializr
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/main/java/com/weg/infoweg/InfoWegApplication.java
@@ -0,0 +1,13 @@
+package com.weg.infoweg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoWegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoWegApplication.class, args);
+	}
+
+}
