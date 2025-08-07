55d4a6c4fc4359c454fd1b2e6881540e53e9dcc3
kotryk <gustavokotryk@gmail.com>
2025-08-06 21:40:20 -0300

    #13 feat: implementa interacao back/front

--- src/main/java/com/weg/infoweg/modules/user/aplication/controller/UserController.java
+++ src/main/java/com/weg/infoweg/modules/user/aplication/controller/UserController.java
@@ -1,18 +1,50 @@
 package com.weg.infoweg.modules.user.aplication.controller;
 
-import com.weg.infoweg.modules.user.aplication.dtos.UserCreateRequest;
-import com.weg.infoweg.modules.user.aplication.dtos.UserCreateResponse;
+import com.weg.infoweg.modules.user.aplication.dtos.*;
 import com.weg.infoweg.modules.user.aplication.port.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserGetResponse> getUser(@PathVariable UserGetRequest userGetRequest) {
+        UserGetResponse response = userService.getUser(userGetRequest);
+        return ResponseEntity.ok(response);
+    }
 
+    @PostMapping
+    public ResponseEntity<UserCreateResponse> createUser(
+            @Valid @RequestBody UserCreateRequest request) {
+        UserCreateResponse response = userService.createUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserUpdateResponse> updateUser(
+            @PathVariable Long id,
+            @Valid @RequestBody UserUpdateRequest request) {
+        UserUpdateResponse response = userService.updateUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserDeleteResponse> deleteUser(@PathVariable Long id) {
+        UserDeleteRequest request = new UserDeleteRequest();
+        UserDeleteResponse response = userService.deleteUser(request);
+        return ResponseEntity.ok(response);
+    }
 }
+
+
