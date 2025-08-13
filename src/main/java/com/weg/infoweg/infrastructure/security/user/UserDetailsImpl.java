68f0739ea28e274ee33192dd06f5cd29c56402a3
Luca5Eckert <ecklukatv@gmail.com>
2025-08-12 21:06:21 -0300

    #35 feat: implementa interface UserDetails em UserDetailsImpl

--- src/main/java/com/weg/infoweg/infrastructure/security/user/UserDetailsImpl.java
+++ src/main/java/com/weg/infoweg/infrastructure/security/user/UserDetailsImpl.java
@@ -1,4 +1,36 @@
 package com.weg.infoweg.infrastructure.security.user;
 
-public class UserDetailsImpl {
+import com.weg.infoweg.modules.user.domain.enums.AccessLevel;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private UUID id;
+    private AccessLevel accessLevel;
+    private String password;
+    private String username;
+
+    public UUID getId(){
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(accessLevel);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
 }
