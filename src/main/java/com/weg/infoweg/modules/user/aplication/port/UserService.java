e5a4aacaa53d3e749bd5490853432d3173a79c2f
Luca5Eckert <ecklukatv@gmail.com>
2025-08-13 14:44:30 -0300

    #35 feat: adiciona UserServiceImpl

--- src/main/java/com/weg/infoweg/modules/user/aplication/port/UserService.java
+++ src/main/java/com/weg/infoweg/modules/user/aplication/port/UserService.java
@@ -2,15 +2,17 @@ package com.weg.infoweg.modules.user.aplication.port;
 
 import com.weg.infoweg.modules.user.aplication.dtos.*;
 
+import java.util.UUID;
+
 
 public interface UserService {
 
-    UserCreateResponse createUser(UserCreateRequest userCreateRequest);
+    UserCreateResponse createUser(UserCreateRequest userCreateRequest, UUID id);
 
-    UserDeleteResponse deleteUser(UserDeleteRequest userDeleteRequest);
+    UserDeleteResponse deleteUser(UserDeleteRequest userDeleteRequest, UUID id);
 
-    UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest);
+    void updateUser(UserUpdateRequest userUpdateRequest, UUID id);
 
-    UserGetResponse getUser(UserGetRequest userGetRequest);
+    UserGetResponse getUser(UserGetRequest userGetRequest, UUID id);
 
 }
--- src/main/java/com/weg/infoweg/modules/user/domain/service/UserServiceImpl.java
+++ src/main/java/com/weg/infoweg/modules/user/domain/service/UserServiceImpl.java
@@ -3,35 +3,43 @@ package com.weg.infoweg.modules.user.domain.service;
 import com.weg.infoweg.modules.user.aplication.dtos.*;
 import com.weg.infoweg.modules.user.aplication.port.UserService;
 import com.weg.infoweg.modules.user.domain.cases.CreateUserCase;
+import com.weg.infoweg.modules.user.domain.cases.DeleteUserCase;
+import com.weg.infoweg.modules.user.domain.cases.UpdateUserCase;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final CreateUserCase createUserCase;
+    private final UpdateUserCase updateUserCase;
+    private final DeleteUserCase deleteUserCase;
 
-    public UserServiceImpl(CreateUserCase createUserCase) {
+    public UserServiceImpl(CreateUserCase createUserCase, UpdateUserCase updateUserCase, DeleteUserCase deleteUserCase) {
         this.createUserCase = createUserCase;
+        this.updateUserCase = updateUserCase;
+        this.deleteUserCase = deleteUserCase;
     }
 
 
     @Override
-    public UserCreateResponse createUser(UserCreateRequest userCreateRequest) {
-        return null;
+    public UserCreateResponse createUser(UserCreateRequest userCreateRequest, UUID id) {
+        return createUserCase.execute(userCreateRequest, id);
     }
 
     @Override
-    public UserDeleteResponse deleteUser(UserDeleteRequest userDeleteRequest) {
-        return null;
+    public UserDeleteResponse deleteUser(UserDeleteRequest userDeleteRequest,  UUID id) {
+        return deleteUserCase.execute(userDeleteRequest, id);
     }
 
     @Override
-    public UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest) {
-        return null;
+    public void updateUser(UserUpdateRequest userUpdateRequest,  UUID id) {
+        updateUserCase.execute(userUpdateRequest, id);
     }
 
     @Override
-    public UserGetResponse getUser(UserGetRequest userGetRequest) {
+    public UserGetResponse getUser(UserGetRequest userGetRequest, UUID id) {
         return null;
     }
 }
