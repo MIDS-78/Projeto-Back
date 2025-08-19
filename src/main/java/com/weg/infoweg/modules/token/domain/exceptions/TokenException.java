1fc5fcabf0076366644847ceb825af256bc18370
Luca5Eckert <ecklukatv@gmail.com>
2025-08-19 17:22:22 -0300

    feat: adiciona exceções para tokens
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/main/java/com/weg/infoweg/modules/token/domain/exceptions/TokenException.java
@@ -0,0 +1,7 @@
+package com.weg.infoweg.modules.token.domain.exceptions;
+
+public class TokenException extends RuntimeException {
+    public TokenException(String message) {
+        super(message);
+    }
+}
