62243ae945f6cd89b163af0d47020f9f179b4694
Luca5Eckert <ecklukatv@gmail.com>
2025-08-19 19:40:37 -0300

    refactor: login utiliza do TokenService

--- src/main/java/com/weg/infoweg/modules/auth/domain/cases/LoginUserCase.java
+++ src/main/java/com/weg/infoweg/modules/auth/domain/cases/LoginUserCase.java
@@ -1,9 +1,11 @@
 package com.weg.infoweg.modules.auth.domain.cases;
 
 import com.weg.infoweg.infrastructure.provider.JwtTokenProvider;
+import com.weg.infoweg.infrastructure.security.user.UserDetailsImpl;
 import com.weg.infoweg.modules.token.application.dtos.JwtTokenDto;
 import com.weg.infoweg.modules.auth.aplication.dtos.login.UserLoginRequest;
 import com.weg.infoweg.modules.auth.domain.exceptions.AuthenticationValidationException;
+import com.weg.infoweg.modules.token.application.port.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,9 +18,9 @@ public class LoginUserCase {
 
     private final AuthenticationManager authenticationManager;
 
-    private final JwtTokenProvider jwtService;
+    private final TokenService jwtService;
 
-    public LoginUserCase(AuthenticationManager authenticationManager, JwtTokenProvider jwtService) {
+    public LoginUserCase(AuthenticationManager authenticationManager, TokenService jwtService) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
     }
@@ -29,9 +31,8 @@ public class LoginUserCase {
         if(authentication.isAuthenticated()){
             throw new AuthenticationValidationException("Invalid credentials");
         }
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
-        String token = jwtService.generateToken(userDetails);
-        return new JwtTokenDto(token);
+        return jwtService.generateToken(userDetails);
     }
 }
