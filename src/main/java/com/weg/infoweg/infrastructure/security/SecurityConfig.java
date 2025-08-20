963d2607d23dfb42589b9f57f94c5a02e36642da
Luca5Eckert <ecklukatv@gmail.com>
2025-08-20 14:44:38 -0300

    #49 refactor: integra CORS com SecurityFilterChain

--- src/main/java/com/weg/infoweg/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/weg/infoweg/infrastructure/security/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,10 +28,14 @@ public class SecurityConfig {
     @Autowired
     private JwtTokenFilter jwtTokenFilter;
 
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource ;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.csrf(AbstractHttpConfigurer::disable)
+        return http.cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
 
