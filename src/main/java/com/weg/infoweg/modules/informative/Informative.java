19b1dd1da4ffac2f28c3a65718b0bc54d8bdb576
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-27 20:22:28 -0300

    #60 feat: cria entidade informative

--- src/main/java/com/weg/infoweg/modules/informative/Informative.java
+++
@@ -1,6 +0,0 @@
-package com.weg.infoweg.modules.informative;
-
-import java.util.UUID;
-
-public class Informative {
-}
---
+++ src/main/java/com/weg/infoweg/modules/informative/domain/Informative.java
@@ -0,0 +1,69 @@
+package com.weg.infoweg.modules.informative.domain;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table (name="Informative")
+public class Informative{
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+        private String description;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] image;
+
+    public Informative(){
+
+    }
+
+    public Informative(UUID id, String title, String description, byte[] image) {
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
