97ef58d423fc46612c1fcccaa3c355ad6f7c01d2
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-27 21:51:35 -0300

    fix: corrige erros de imports

---
+++ src/main/java/com/weg/infoweg/modules/informative/domain/exception/UserWithoutPermissionInformativeException.java
@@ -0,0 +1,7 @@
+package com.weg.infoweg.modules.informative.domain.exception;
+
+public class UserWithoutPermissionInformativeException extends InformativeException {
+    public UserWithoutPermissionInformativeException(String message) {
+        super(message);
+    }
+}
